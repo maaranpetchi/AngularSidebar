c55a5c6ae99ac17bcc94a0870a8005b5cd58d5a7
maaranpetchi <maaranfamily22@gmail.com>
2023-06-02 15:56:12 +0530

    Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularSidebar';
+  menuItems = [
+    {
+      title: 'Menu 1',
+      active: false,
+      subMenuItems: [
+        { title: 'Sub Menu 1' },
+        { title: 'Sub Menu 2' },
+        { title: 'Sub Menu 3' }
+      ]
+    },
+    {
+      title: 'Menu 2',
+      active: false,
+      subMenuItems: [
+        { title: 'Sub Menu 4' },
+        { title: 'Sub Menu 5' }
+      ]
+    },
+    {
+      title: 'Menu 3',
+      active: false,
+      subMenuItems: [
+        { title: 'Sub Menu 6' },
+        { title: 'Sub Menu 7' },
+        { title: 'Sub Menu 8' }
+      ]
+    }
+  ];
+
+  toggleSubMenu(item: any): void {
+    item.active = !item.active;
+  }
 }
